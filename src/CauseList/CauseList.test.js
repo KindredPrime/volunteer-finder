d851e124850af3eb329e6319c12757e5a88066d3
Ken Wright <kenneth.gordon.wright@gmail.com>
2020-11-07 22:10:44 -0500

    Update CauseList tests

--- src/CauseList/CauseList.test.js
+++ src/CauseList/CauseList.test.js
@@ -1,18 +1,23 @@
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import CauseList from './CauseList';
+import { dummyCauses } from '../dummyData';
 
 describe('CauseList Component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
-      <CauseList causes={[
-        {
-          id: 0,
-          name: 'name'
-        }
-      ]}/>,
+      <CauseList causes={[]}/>,
       div
     );
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    render(
+      <CauseList causes={dummyCauses} />
+    );
+
+    expect(document.body).toMatchSnapshot();
   });
 });
