4b8736baaa4ec875a66fdaf81bdab04546ce23e3
Ken Wright <kenneth.gordon.wright@gmail.com>
2020-11-14 16:33:57 -0500

    Update homepage to make app purpose more clear

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,25 +5,33 @@ function Home() {
   return (
     <div className="Home">
       <Nav />
-      <header className="page-title">
-        <h1>Welcome to VolunteerFinder</h1>
-      </header>
+      <main>
+        <header className="page-title">
+          <h1>Welcome to VolunteerFinder</h1>
+        </header>
 
-      <section>
-        <h2>Find Volunteer Organizations</h2>
-        <p>
-          Search for organizations in the DC metropolitan area by name, cause(s), 
-          and any relevant tags.
+        <p className="Home__statement">
+          Looking to volunteer in the DC metropolitan area for causes you beleive in, but you're 
+          having trouble finding volunteer opportunities? VolunteerFinder can help you!
         </p>
-      </section>
 
-      <section>
-        <h2>Create an Account and Add Organizations</h2>
-        <p>
-          Each organization includes a name, contact info, the cause(s) it’s involved with, and any
-          other miscellaneous tags.
-        </p>
-      </section>
+        <section>
+          <h2>Find Volunteer Organizations</h2>
+          <p>
+            Search for organizations by name, address, description, cause(s), and any relevant 
+            tags.
+          </p>
+        </section>
+
+        <section>
+          <h2>Add Organizations</h2>
+          <p>
+            Know of a great organization for volunteering, but you don't see it here? You can 
+            create an account and add it yourself!  To help make sure users can find it, give as 
+            much info as you can, and assign tags to it so it will easily show up in searches.
+          </p>
+        </section>
+      </main>
     </div>
   );
 }
